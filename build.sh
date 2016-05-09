ab69f2784ec64275e98c3bd06fa1493b6ae9c6af
michael austin <ma@ixis.co.uk>
2016-05-09 15:03:12 +0100

    adding initial version

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# if makefile detected proceed with drush make
+if [ -f make.yml ];
+then
+  echo -e "Found a makefile, proceeding with drush make ..."
+  drush make make.yml webapp -y
+else
+  echo "No makefile found, proceeding with standard build ..."
+fi
+
+# copy drupal settings.php file into place
+cp settings.php webapp/sites/default/
+
+# archive project code ready for deploy
+tar -czf webapp.tar.gz webapp
+
+# deploy the archive to S3 (variables defined from .drone.yml using drone secrets)
+aws s3 cp webapp.tar.gz s3://$AWS_S3_BUCKET_NAME/$(basename $PWD)-$CI_BRANCH-latest.tar.gz --acl public-read --region $AWS_REGION
