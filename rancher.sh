c11addc9f4bc14e9bf592e020569aadcd6c1c395
michael austin <ma@ixis.co.uk>
2016-05-17 14:03:54 +0100

    Removing region and s3 settings

--- rancher.sh
+++ rancher.sh
@@ -11,12 +11,6 @@ sed -i -e "s/CIBRANCH/$CI_BRANCH/g" rancher-compose.yml docker-compose.yml
 # set the correct project name in the compose files
 sed -i -e "s/REPO/$(basename $PWD)/g" rancher-compose.yml docker-compose.yml
 
-# set the correct AWS region in the compose files
-sed -i -e "s/AWSREGION/$AWS_REGION/g" rancher-compose.yml docker-compose.yml
-
-# set the correct S3 URL in the compose files
-sed -i -e "s/S3URL/$S3_URL/g" rancher-compose.yml docker-compose.yml
-
 # provision the environment within Rancher
 rancher-compose -p $(basename $PWD)-$CI_BRANCH up -u -c -d --force-upgrade
 
