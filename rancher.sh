e06cee05a7bc61ee28e76ee4f974f9e62a3bac0a
michael austin <ma@ixis.co.uk>
2016-05-23 15:21:17 +0100

    new variables

--- rancher.sh
+++ rancher.sh
@@ -12,10 +12,10 @@ sed -i -e "s/CIBRANCH/$CI_BRANCH/g" rancher-compose.yml docker-compose.yml
 #sed -i -e "s/REPO/$(basename $PWD)/g" rancher-compose.yml docker-compose.yml
 
 # set the correct organisation/repo name in the compose files with dash
-sed -i -e "s/CIREPO2/$(echo $CI_REPO | sed 's/\//-/g')/g" rancher-compose.yml docker-compose.yml
+sed -i -e "s/ORGDASHREPO/$(echo $CI_REPO | sed 's/\//-/g')/g" rancher-compose.yml docker-compose.yml
 
 # set the correct organisation/repo name in the compose files with slash (using : as delimiter as variable contains slashes)
-sed -i -e "s:CIREPO:$CI_REPO:g" rancher-compose.yml docker-compose.yml
+sed -i -e "s:ORGSLASHREPO:$CI_REPO:g" rancher-compose.yml docker-compose.yml
 
 # provision the environment within Rancher
 rancher-compose -p $(echo $CI_REPO | sed 's/\//-/g')-$CI_BRANCH up -u -c -d --force-upgrade
