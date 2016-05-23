4167a8c4c2f4be60b46e011c4ff428a69f673f44
michael austin <ma@ixis.co.uk>
2016-05-23 14:15:39 +0100

    add organisation name amends

--- rancher.sh
+++ rancher.sh
@@ -11,8 +11,11 @@ sed -i -e "s/CIBRANCH/$CI_BRANCH/g" rancher-compose.yml docker-compose.yml
 # set the correct project name in the compose files
 sed -i -e "s/REPO/$(basename $PWD)/g" rancher-compose.yml docker-compose.yml
 
+# set the correct organisation name in the compose files
+sed -i -e "s/ORGANISATION/$ORGANISATION_NAME/g" rancher-compose.yml docker-compose.yml
+
 # provision the environment within Rancher
-rancher-compose -p $(basename $PWD)-$CI_BRANCH up -u -c -d --force-upgrade
+rancher-compose -p $ORGANISATION-$(basename $PWD)-$CI_BRANCH up -u -c -d --force-upgrade
 
 # end time
 date
