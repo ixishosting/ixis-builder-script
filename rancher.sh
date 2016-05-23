81aaea44a26c81cf5e12aa1edfbc03a1c4ee16c2
michael austin <ma@ixis.co.uk>
2016-05-23 14:59:24 +0100

    amendments to how organisation repo set

--- rancher.sh
+++ rancher.sh
@@ -9,10 +9,13 @@ date
 sed -i -e "s/CIBRANCH/$CI_BRANCH/g" rancher-compose.yml docker-compose.yml
 
 # set the correct project name in the compose files
-sed -i -e "s/REPO/$(basename $PWD)/g" rancher-compose.yml docker-compose.yml
+#sed -i -e "s/REPO/$(basename $PWD)/g" rancher-compose.yml docker-compose.yml
 
-# set the correct organisation name in the compose files
-sed -i -e "s/ORGANISATION/$ORGANISATION_NAME/g" rancher-compose.yml docker-compose.yml
+# set the correct organisation/repo name in the compose files with dash
+sed -i -e "s/CIREPO2/$(echo $CI_REPO | sed 's/\//-/g')/g" rancher-compose.yml docker-compose.yml
+
+# set the correct organisation/repo name in the compose files with slash
+sed -i -e "s/CIREPO/$CI_REPO/g" rancher-compose.yml docker-compose.yml
 
 # provision the environment within Rancher
 rancher-compose -p $ORGANISATION_NAME-$(basename $PWD)-$CI_BRANCH up -u -c -d --force-upgrade
