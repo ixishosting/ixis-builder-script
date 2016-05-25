ddf25d5422d1a420a6ad5746f03b23cecdcae1ee
michael austin <ma@ixis.co.uk>
2016-05-25 16:07:13 +0100

    adding project config file support

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+###
 # a build script for usage with ixis docker build system
+###
 
-# start time
+### start time ###
 date
 
-# if makefile detected proceed with drush make
+### if makefile detected proceed with drush make ###
 if [ -f make.yml ];
 then
   echo -e "Found a makefile, proceeding with drush make ..."
@@ -14,14 +16,17 @@ else
   echo "No makefile found, proceeding with standard build ..."
 fi
 
-# copy drupal settings.php file into place
+### copy drupal settings.php file into place ###
 cp settings.php webapp/sites/default/
 
-# archive project code ready for deploy
+### copy project .project.yml file into place ###
+cp .project.yml webapp/
+
+### archive project code ready for deploy ###
 tar -czf webapp.tar.gz webapp
 
-# deploy the archive to S3 (variables defined from .drone.yml using drone secrets)
+### deploy the archive to S3 (variables defined from .drone.yml using drone secrets) ###
 aws s3 cp webapp.tar.gz s3://$AWS_S3_BUCKET_NAME/$(echo $CI_REPO | sed 's/\//-/g')-$CI_BRANCH-latest.tar.gz --acl public-read --region $AWS_REGION
 
-# end time
+### end time ###
 date
