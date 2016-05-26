f1b61e8a45b6c085c860014b3ecca4136e53f77b
michael austin <ma@ixis.co.uk>
2016-05-26 14:18:02 +0100

    amend renaming

--- rancher.sh
+++ rancher.sh
@@ -14,7 +14,7 @@ sed -i -e "s/CIBRANCH/$CI_BRANCH/g" rancher-compose.yml docker-compose.yml
 sed -i -e "s/CIORG/$(echo $CI_REPO |cut -d'/' -f1)/g" rancher-compose.yml docker-compose.yml
 
 ### set the correct repository name in the compose files ###
-sed -i -e "s/CIORG/$(echo $CI_REPO |cut -d'/' -f2)/g" rancher-compose.yml docker-compose.yml
+sed -i -e "s/CIREPO/$(echo $CI_REPO |cut -d'/' -f2)/g" rancher-compose.yml docker-compose.yml
 
 ### provision the environment within Rancher (branch-repo-organisation)###
 rancher-compose -p $CI_BRANCH-$(echo $CI_REPO |cut -d'/' -f2)-$(echo $CI_REPO |cut -d'/' -f1) up -u -c -d --force-upgrade
